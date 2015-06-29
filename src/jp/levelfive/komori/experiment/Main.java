6cbb7d90aabc425078a93056c3ded0003f112774
Hiroaki Komori <yukikaze.inside@gmail.com>
2015-06-29 17:06:39 +0900

    first commit

---
+++ src/jp/levelfive/komori/experiment/Main.java
@@ -0,0 +1,11 @@
+package jp.levelfive.komori.experiment;
+
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SimpleRegression regression = new SimpleRegression();
+	}
+
+}
